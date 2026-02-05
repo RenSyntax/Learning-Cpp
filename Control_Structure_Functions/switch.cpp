ee8efd238727f64977b1978a2b1aefeee91bb885
RenSyntax <ren.syntax@gmail.com>
2026-02-05 21:59:51 +0530

    Learning

---
+++ Control_Structure_Functions/switch.cpp
@@ -0,0 +1,34 @@
+// Switch Statement:
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int userAge{};
+    
+    cout << "Please Enter Your Age." << endl;
+    cin >> userAge;
+
+    // looks cleaner than if/else, but still i prefer if/else.
+    // switch statements are prefered if condition has constant.
+
+    switch (userAge) {
+        case 18:
+        cout << "You Are Legally A Adult." << endl;
+        break;
+        case 12:
+        cout << "Sorry, Kids not allowded on this website" << endl;
+        break;
+        case 60:
+        cout << "Thanks for using my program" << endl;
+        [[fallthrough]];
+        case 61:
+        cout << "Visit Again" << endl;
+        break;
+        default:
+        cout << "Sorry, your age doesn't match the data base";
+
+    }
+    return 0;
+
+}
