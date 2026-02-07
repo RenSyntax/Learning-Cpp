4bf12ce333d3b96a6b73ebbae1be7101fcc4164e
RenSyntax <ren.syntax@gmail.com>
2026-02-07 21:35:31 +0530

    Debug

--- Control_Structure_Functions/art_generator.cpp
+++ Control_Structure_Functions/art_generator.cpp
@@ -14,9 +14,14 @@ int main() {
         // but since we have added this in another loop of 5times, the actuall  total time this loop will run is 3*5 = 15times.
         // for the previous loop, this loop runing 3 times is equivalent of runing this code only 1 time, so it repeats that this 5times, 
         // thus making it 15 times. 
-        for(int j = 1; j <= 3; j++) {
-            cout << "****************" << endl;
+        for(int j = 1; j <= 10; j++) {
+            cout << "*";
         }
+        
+        // after writing this endl, now i understand why loops are usefull.
+        // think this way, first loop is limiting the number of colomuns.
+        // and the second loop, is limiting how many starts will be there is one colomun.
+        cout << endl;
 
 
     }
