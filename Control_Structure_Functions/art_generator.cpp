841bd74b0a94fe5556def2ca5798f72bd7f77849
RenSyntax <ren.syntax@gmail.com>
2026-02-07 21:08:09 +0530

    Work

---
+++ Control_Structure_Functions/art_generator.cpp
@@ -0,0 +1,16 @@
+// Making a triangle
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int x{5};
+
+    for(int i = 1; i <= x; i++) {
+
+
+        cout << star << endl;
+    }
+
+
+}
