1759b2fd0fde4139f8eb0023b33198d3aefe8b2c
RenSyntax <ren.syntax@gmail.com>
2026-02-07 21:24:34 +0530

    Explaination

--- Control_Structure_Functions/art_generator.cpp
+++ Control_Structure_Functions/art_generator.cpp
@@ -1,15 +1,24 @@
-// Making a triangle
+// Learning Loops, through a test program:
 
 #include <iostream>
 using namespace std;
 
 int main() {
     int x{5};
-
+    //Explaination: (i am still doubtful but here is what i thnk is happening)
+    
+    // this basically tells compiler to run the below code 'x' number of times.  
     for(int i = 1; i <= x; i++) {
+       
+        // this tells compiler to run this loop for 3times.
+        // but since we have added this in another loop of 5times, the actuall  total time this loop will run is 3*5 = 15times.
+        // for the previous loop, this loop runing 3 times is equivalent of runing this code only 1 time, so it repeats that this 5times, 
+        // thus making it 15 times. 
+        for(int j = 1; j <= 3; j++) {
+            cout << "****************" << endl;
+        }
 
 
-        cout << star << endl;
     }
 
 
