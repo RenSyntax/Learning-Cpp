6b5d250b459be1ffad69b49f838a2ade98dcb293
RenSyntax <ren.syntax@gmail.com>
2026-02-10 23:08:37 +0530

    Add files via upload

---
+++ Control_Structure_Functions/function.cpp
@@ -0,0 +1,28 @@
+// This is just the application of what i have learned in lectures.
+
+#include <iostream>
+using namespace std;
+#include <string>
+
+string hello(int x, int y) {
+    int z{1};
+    while(z <= x) {
+        cout << "Hello" << endl;
+        z++;
+    }
+    int l{1};
+    while(l <=y) {
+        cout << "World!!" << endl;
+        l++;
+        }
+    return 0;
+     }
+
+int main() {
+    int first{};
+    int second{};
+    cout << "How Many times you wana print, Hello & World" << endl;
+    cin >> first >> second;
+    cout << hello(first,second);
+    return 0;
+}
