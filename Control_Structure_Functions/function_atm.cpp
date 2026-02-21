b86186f4976be4592cc768dee0e553671a332c4e
RenSyntax <ren.syntax@gmail.com>
2026-02-21 22:29:06 +0530

    Rename

---
+++ Control_Structure_Functions/function_atm.cpp
@@ -0,0 +1,32 @@
+// In this i am gona Learn about functions:
+
+#include <iostream>
+using namespace std;
+int pin{};
+
+// So, function are basically automation scrips.
+// It lets you automate a specific function in your program so you dont have
+// have to copy and paste code everytime.
+
+// Here is a good Example, i will try to make a mini ATM simulator with
+// functions.
+
+bool cheakPin(int a) {
+  int i{0};
+  while (i <= 3) {
+    if (a == pin) {
+      return true;
+    } else {
+      return false;
+    }
+  }
+}
+
+int main() {
+  int userInput{};
+  cout << "Press 1: Deposit\n";
+  cout << "Press 2: Withdraw\n";
+  cin >> userInput;
+
+  cout << ""
+}
