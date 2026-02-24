9c4be34b79601b7f8a690bb64264fea2581ebf9f
RenSyntax <ren.syntax@gmail.com>
2026-02-24 21:40:21 +0530

    Core

---
+++ Control_Structure_Functions/function_overloading.cpp
@@ -0,0 +1,21 @@
+// function overloading is nothing but a very small concept.
+// when you have to two functions whose core working is same, but the inputs or
+// outputs are little diffrent, what you can do is you can name those function
+// the same name and it will work just fine, becasue cpp will diffrensiate the
+// functions based on inputs type..
+
+// Here is a Example:
+
+#include <iostream>
+using namespace std;
+
+int add(int a, int b) { return a + b; }
+
+double add(double a, double b) { return a + b; }
+
+// here you can see the
+int main() {
+  cout << add(3, 4) << "\n";
+  cout << add(3.4, 4.5) << "\n";
+  return 0;
+}
