bd760bdc216d6818c63951c79e5e215573b603b8
RenSyntax <ren.syntax@gmail.com>
2026-02-26 22:52:55 +0530

    Working Code with bugs

--- Control_Structure_Functions/attendence_calculator.cpp
+++ Control_Structure_Functions/attendence_calculator.cpp
@@ -3,3 +3,61 @@
 #include <iostream>
 #include <string>
 using namespace std;
+
+int attendence{};
+int totalClass{};
+
+int calculatePercentage(int i) {
+
+  int percentage{(attendence / i) * 100};
+  return percentage;
+}
+
+int main() {
+
+  while (true) {
+    int userInput{};
+    int lectures{};
+    cout << "Press 1: Add Attendence\n";
+    cout << "Press 2: Remove Attendence\n";
+    cout << "Press 3: Calculate Percentage\n";
+    cout << "Press 4: See Your Total Attendence\n";
+    cin >> userInput;
+
+    switch (userInput) {
+    case 1: {
+      cout << "Enter The Number Of Lectures Attempted Today: ";
+      cin >> lectures;
+
+      attendence += lectures;
+      cout << "Attendence Added Sucsessfully.\n";
+      break;
+    }
+
+    case 2: {
+      cout << "Enter The Number of Lectures, You Wana Remove: ";
+      cin >> lectures;
+
+      attendence -= lectures;
+      cout << "Attendence Updated.\n";
+      break;
+    }
+
+    case 3: {
+      cout << "Enter Total Number Of Lectures: \n";
+      cin >> totalClass;
+      cout << "Your Attendence Percentage is: "
+           << calculatePercentage(totalClass) << endl;
+      break;
+    }
+    case 4: {
+      cout << "Your Total Attendence is: " << attendence << endl;
+      break;
+    }
+    default: {
+      cout << "Please Enter A Valid Input..\n";
+    }
+    }
+  }
+  return 0;
+}
