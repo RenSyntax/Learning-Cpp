66a9b8fb943d980d9380f90ce8dca9e65af7210a
RenSyntax <ren.syntax@gmail.com>
2026-03-01 20:58:09 +0530

    Time OUT

--- Control_Structure_Functions/calculator_new.cpp
+++ Control_Structure_Functions/calculator_new.cpp
@@ -2,3 +2,19 @@
 
 #include <iostream>
 using namespace std;
+
+void operatorDisplay() {
+  cout << "------------------------------\n";
+  cout << "Please Enter Your Operator: \n";
+  cout << "Press 1: Addition\nPress 2: Subtraction\nPress 3: "
+          "Multiplication\nPress 4: Division\n Press 5: Equal to\n"
+}
+int main() {
+  while (true) {
+    cout << "Please Enter Your Number: ";
+    cin >> num1;
+
+    operatorDisplay();
+    cin >> operator;
+  }
+}
