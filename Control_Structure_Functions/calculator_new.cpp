2c46d9cdf0b4eec2acaf0ca09b428fb22ebb6e95
RenSyntax <ren.syntax@gmail.com>
2026-03-01 20:47:22 +0530

    initial

---
+++ Control_Structure_Functions/calculator_new.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+inline int add(int x, int y) { return x + y; }
+
+inline int subtract(int x, int y) { return x - y; }
+
+inline int multiply(int x, int y) { return x * y; }
+
+inline int divide(int x, int y) { return x / y; }
